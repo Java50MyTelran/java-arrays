6b2fda660c7b947ff641cf362bfcb3ad26091f02
Yuragranovsky <yuragranovsky@gmail.com>
2023-05-30 21:13:32 +0300

    CW #2 Arrays

---
+++ src/telran/arrays/ArraysInt.java
@@ -0,0 +1,28 @@
+package telran.arrays;
+
+import java.util.Arrays;
+
+public class ArraysInt {
+public static int[] addNumber(int [] array, int number) {
+	int[] res = Arrays.copyOf(array, array.length + 1);
+	res[array.length] = number;
+	return res;
+}
+public static int[] insertNumber(int[] array, int index, int number) {
+	int [] res = new int[array.length + 1];
+	System.arraycopy(array, 0, res, 0, index);
+	res[index] = number;
+	System.arraycopy(array, index, res, index + 1, array.length - index);
+	return res;
+}
+public static int[] removeNumber(int [] array, int index) {
+	//TODO returns array with no number at the given index
+	return null;
+}
+public static int[] insertNumberSorted(int [] arraySorted, int number) {
+	//TODO takes sorted array and number to insert
+	//returns sorted array with the given number at a proper position
+	//apply the standard method of the class Arrays int binarySearch(int[] arraySorted, int key)
+	return null;
+}
+}
